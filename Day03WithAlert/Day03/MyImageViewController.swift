3fe5eb3ee974f18fc180b682cb65e6ab4df0320e
wedwer666 <mitriucmaria@gmail.com>
2017-04-20 20:58:21 +0300

    Add files via upload

---
+++ Day03WithAlert/Day03/MyImageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MyImageViewController.swift
+//  Day03
+//
+//  Created by Mitriuc Maria on 4/20/17.
+//  Copyright © 2017 Maria Mitriuc. All rights reserved.
+//
+
+import UIKit
+
+class MyImageViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
